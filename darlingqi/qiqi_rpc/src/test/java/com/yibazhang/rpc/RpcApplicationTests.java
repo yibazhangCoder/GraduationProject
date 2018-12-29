d97ba3de1a47ab8159f7f3841207886be03dc36c
yibazhang <wangshiyibazhang@gmail.com>
2018-12-29 14:24:43 +0800

    项目初始化

---
+++ darlingqi/qiqi_rpc/src/test/java/com/yibazhang/rpc/RpcApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yibazhang.rpc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RpcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
